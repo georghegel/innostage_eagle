eae35061a1e5ed261e79221afd217afa2c40eb67
YarBurArt <yaroslav.chayk@gmail.com>
2025-07-31 13:25:21 +0500

    add models for chains and mythic agents

--- backend/app/models.py
+++ backend/app/models.py
@@ -16,7 +16,10 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import BigInteger, Boolean, DateTime, ForeignKey, String, Uuid, func
+from sqlalchemy import (
+    BigInteger, Boolean, DateTime, ForeignKey, 
+    String, Uuid, func, Text, Integer
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -35,12 +38,19 @@ class User(Base):
     user_id: Mapped[str] = mapped_column(
         Uuid(as_uuid=False), primary_key=True, default=lambda _: str(uuid.uuid4())
     )
+    username: Mapped[str] = mapped_column(
+        String(256), nullable=True, unique=True, index=True
+    )
+    role: Mapped[str] = mapped_column(
+        String(64), nullable=True, unique=False
+    )
     email: Mapped[str] = mapped_column(
         String(256), nullable=False, unique=True, index=True
     )
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
     refresh_tokens: Mapped[list["RefreshToken"]] = relationship(back_populates="user")
-
+    attack_chain: Mapped[list["AttackChain"]] = relationship(back_populates="user")
+    
 
 class RefreshToken(Base):
     __tablename__ = "refresh_token"
@@ -55,3 +65,85 @@ class RefreshToken(Base):
         ForeignKey("user_account.user_id", ondelete="CASCADE"),
     )
     user: Mapped["User"] = relationship(back_populates="refresh_tokens")
+
+
+class AttackChain(Base):
+    __tablename__ = "attack_chain"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("user_account.user_id", ondelete="CASCADE"),
+    )
+    chain_name: Mapped[str] = mapped_column(
+        String(256), nullable=False, unique=True
+    )
+    final_status: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    user: Mapped["User"] = relationship(back_populates="attack_chain")
+    attack_step: Mapped[list["AttackStep"]] = relationship(back_populates="attack_chain")
+    
+
+class AttackStep(Base):
+    __tablename__ = "attack_step"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    chain_id: Mapped[str] = mapped_column(
+        ForeignKey("attack_chain.id", ondelete="CASCADE"),
+    )
+    phase: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    tool_name: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    command: Mapped[str] = mapped_column(
+        Text, nullable=False, unique=False
+    )
+    mythic_task_id: Mapped[int] = mapped_column(
+        Integer, nullable=False, unique=False
+    )
+    # only for downloading payload link
+    mythic_payload_uuid: Mapped[str] = mapped_column(
+        Uuid(as_uuid=False) , nullable=False, unique=False
+    )
+    mythic_payload_id: Mapped[int] = mapped_column(
+        Integer, nullable=False, unique=False
+    )
+    # raw command output from task, without llm
+    raw_log: Mapped[str] = mapped_column(
+        Text, nullable=True, unique=False
+    )
+    status: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    executed_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+        
+    attack_chain: Mapped["AttackChain"] = relationship(back_populates="attack_step")
+    agent: Mapped[list["Agent"]] = relationship(back_populates="attack_step")
+
+
+class Agent(Base):
+    __tablename__ = "agent"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    step_id: Mapped[str] = mapped_column(
+        ForeignKey("attack_step.id", ondelete="CASCADE"),
+    )
+    agent_name: Mapped[str] = mapped_column(
+        String(256), nullable=False, unique=True
+    )
+    os_type: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    status: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=False
+    )
+    # id from mythic for performing tasks
+    callback_display_id: Mapped[int] = mapped_column(
+        Integer, nullable=False, unique=False
+    )
+    attack_step: Mapped["AttackStep"] = relationship(back_populates="agent")
+
